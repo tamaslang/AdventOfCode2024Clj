f3b69428b580b5621f21283a3cb17625c068d00a
Tamas <tamas@talangsoft.org>
2024-12-02 14:42:01 +0100

    Solve day 2 / tidy up

--- src/com/day2/SafeReports.clj
+++ src/com/day2/SafeReports.clj
@@ -15,39 +15,19 @@
    (filter #(or (numbers-safe? increasing %1) (numbers-safe? decreasing %1)))
    (count)))
 
-(defn numbers-safe-dampener? [direction numbers]
-  (let
-   [[_ errors] (reduce (fn [[last-nr errors], current-nr]
-                         (let
-                          [safe? (direction last-nr current-nr)
-                           errors* (if safe? errors (inc errors))
-                           last-nr* (if safe? current-nr last-nr)]
-                           [last-nr* errors*])) [(first numbers) 0] (rest numbers))]
-    (<= errors 1)))
-
-(defn number-of-safe-reports-with-dampener
-  [data]
-  (->>
-   data
-   (map parse-numbers-in-line)
-   (filter #(or (numbers-safe-dampener? increasing %1) (numbers-safe-dampener? decreasing %1)))
-   (count)))
-
 (defn all-combinations-with-one-removed [numbers]
   (->>
    (range 0 (count numbers))
    (map (fn [index] (flatten (keep-indexed #(when-not (= index %1) %2) numbers))))))
 
 (defn has-a-valid-combination [lists-of-numbers]
-  (some (fn [combinations-for-1] (or (numbers-safe? increasing combinations-for-1) (numbers-safe? decreasing combinations-for-1))) lists-of-numbers))
+  (some (fn [combinations-for-one] (or (numbers-safe? increasing combinations-for-one) (numbers-safe? decreasing combinations-for-one))) lists-of-numbers))
 
 (defn number-of-safe-reports-with-dampener-brute-force
   [data]
   (let [{safe-rows true unsafe-rows false} (->>
-                                        data
-                                        (map parse-numbers-in-line)
-                                        (group-by #(or (numbers-safe? increasing %1) (numbers-safe? decreasing %1))))
-        safe-when-dampener (filter (fn[unsafe-row] (has-a-valid-combination (all-combinations-with-one-removed unsafe-row))) unsafe-rows)
-        ]
-
+                                            data
+                                            (map parse-numbers-in-line)
+                                            (group-by #(or (numbers-safe? increasing %1) (numbers-safe? decreasing %1))))
+        safe-when-dampener (filter (fn [unsafe-row] (has-a-valid-combination (all-combinations-with-one-removed unsafe-row))) unsafe-rows)]
     (+ (count safe-rows) (count safe-when-dampener))))
--- test/com/day2/SafeReports_test.clj
+++ test/com/day2/SafeReports_test.clj
@@ -20,19 +20,6 @@
   (testing "Should find safe reports in input file"
     (is (= 549 (number-of-safe-reports (str/split-lines (slurp data-file)))))))
 
-(deftest should-find-safe-reports-with-dampener
-  (testing "Should find safe reports with dampener"
-    (is (= 4 (number-of-safe-reports-with-dampener ["7 6 4 2 1"
-                                                    "1 2 7 8 9"
-                                                    "9 7 6 2 1"
-                                                    "1 3 2 4 5"
-                                                    "8 6 4 4 1"
-                                                    "1 3 6 7 9"])))))
-
-(deftest should-find-safe-reports-with-dampener-on-input-file
-  (testing "Should find safe reports with dampener in input file"
-    (is (= 579 (number-of-safe-reports-with-dampener (str/split-lines (slurp data-file)))))))
-
 (deftest should-find-safe-reports-with-dampener-brute-force-on-input-file
   (testing "Should find safe reports with dampener brute force"
     (is (= 589 (number-of-safe-reports-with-dampener-brute-force (str/split-lines (slurp data-file)))))))
