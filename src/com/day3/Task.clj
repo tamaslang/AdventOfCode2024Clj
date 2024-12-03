c2f41759ca2b753b0f06edd9ed96d9d17685dd1c
Tamas <tamas@talangsoft.org>
2024-12-03 09:57:17 +0100

    solve day 3 task 2

--- src/com/day3/Task.clj
+++ src/com/day3/Task.clj
@@ -9,6 +9,22 @@
      instructions
      (map #(re-seq #"mul\((\d+),(\d+)\)" %1))
      (map first)
-     (map (fn [[_ left right]] (* (Integer/parseInt left) (Integer/parseInt right))))
+     (map (fn [[_ x y]] (* (Integer/parseInt x) (Integer/parseInt y))))
      (reduce +))))
 
+(defn solve-mul [mul-str]
+  (let [[_ x y] (first (re-seq #"mul\((\d+),(\d+)\)" mul-str))]  (* (Integer/parseInt x) (Integer/parseInt y))))
+
+(defn solve2
+  "should find solution"
+  [data]
+  (let [instructions (re-seq #"(?:don\'t)|(?:do)|(?:mul\(\d+,\d+\))" data)]
+    (first (reduce (fn [[total do?], current]
+                     (cond
+                       (= current "don't") [total false]
+                       (= current "do") [total true]
+                       do? [(+ total (solve-mul current)) do?]
+                       :else [total do?]))
+
+                   [0 true]
+                   instructions))))
--- test/com/day3/Task_test.clj
+++ test/com/day3/Task_test.clj
@@ -15,3 +15,11 @@
 (deftest should-solve-for-input-file
   (testing "Should solve for input file"
     (is (= 164730528 (solve (slurp data-file))))))
+
+(deftest should-solve2-in-example
+  (testing "Should solve2"
+    (is (= 48 (solve2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")))))
+
+(deftest should-solve2-for-input-file
+  (testing "Should solve2 for input file"
+    (is (= 164730528 (solve2 (slurp data-file))))))
