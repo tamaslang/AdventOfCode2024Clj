0f531963f8cc3b70de3eba10610e3ac291adaf3c
Tamas <tamas@talangsoft.org>
2024-12-09 13:34:04 +0100

    Day 9 - solve for task1 (refactor but stack overflow!)

--- src/com/day9/Task.clj
+++ src/com/day9/Task.clj
@@ -1,39 +1,39 @@
 (ns com.day9.Task
   (:require [com.utils.InputParsing :refer :all]))
 
-(defn checksum-for-memory [memory]
+(defn expand-memory [memory]
   (loop
    [memory memory
-    logical-idx 0
     idx 0
     move-idx (dec (count memory))
-    total 0]
-    (println inc "|" move-idx)
+    expanded-memory []]
     (cond
-      (> idx move-idx) total
+      (> idx move-idx) expanded-memory
       (even? idx) ; FILE!
       (let [file-size (nth memory idx)
-            logical-idx* (+ logical-idx file-size)
             file-id (/ idx 2)
-            checksum (reduce + (map #(* file-id %1) (range logical-idx logical-idx*)))]
-        (recur memory logical-idx* (inc idx) move-idx (+ total checksum)))
+            expanded (repeat file-size file-id)]
+        (recur memory (inc idx) move-idx (concat expanded-memory expanded)))
       (odd? idx) ; SPACE!
       (let [file-id (/ move-idx 2)
             space-size (nth memory idx)
             move-size (nth memory move-idx)
-            logical-idx* (min (+ logical-idx space-size) (+ logical-idx move-size))
-            checksum (reduce + (map #(* file-id %1) (range logical-idx logical-idx*)))
+            movable-size (min space-size move-size)
+            expanded (repeat movable-size file-id)
             move-idx* (if (>= space-size move-size) (- move-idx 2) move-idx)
             remaining* (if (<= space-size move-size) (- move-size space-size) 0)
             idx* (if (> space-size move-size) idx (inc idx))
             space-size-remaining (if (> space-size move-size) (- space-size move-size) 0)
             memory* (assoc memory move-idx remaining* idx space-size-remaining)]
-        (recur memory* logical-idx* idx* move-idx* (+ total checksum))))))
+        (recur memory* idx* move-idx* (concat expanded-memory expanded))))))
 
 (defn calculate-checksum-for-memory
   "should find solution"
   [data]
-
   (println "data=" data)
   (let [memory (into [] (map #(- (int %1) (int \0))) data)]
-    (checksum-for-memory memory)))
+    (->>
+     (expand-memory memory)
+     (map-indexed vector)
+     (map (fn [[idx id]] (* idx id)))
+     (reduce +))))
