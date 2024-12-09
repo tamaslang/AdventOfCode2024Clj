8566f05ebb2e6d669cebc657d61a4f329ca8a7d3
Tamas <tamas@talangsoft.org>
2024-12-09 18:47:24 +0100

    Day 9 - solve for task2

--- src/com/day9/Task.clj
+++ src/com/day9/Task.clj
@@ -1,39 +1,75 @@
 (ns com.day9.Task
   (:require [com.utils.InputParsing :refer :all]))
 
-(defn expand-memory [memory]
+; https://www.educative.io/answers/vectors-vs-lists-in-clojure
+
+(defn checksum-for-memory [memory]
   (loop
    [memory memory
+    logical-idx 0
     idx 0
     move-idx (dec (count memory))
-    expanded-memory []]
+    total 0]
+    (println inc "|" move-idx)
     (cond
-      (> idx move-idx) expanded-memory
+      (> idx move-idx) total
       (even? idx) ; FILE!
       (let [file-size (nth memory idx)
+            logical-idx* (+ logical-idx file-size)
             file-id (/ idx 2)
-            expanded (repeat file-size file-id)]
-        (recur memory (inc idx) move-idx (concat expanded-memory expanded)))
+            checksum (reduce + (map #(* file-id %1) (range logical-idx logical-idx*)))]
+        (recur memory logical-idx* (inc idx) move-idx (+ total checksum)))
       (odd? idx) ; SPACE!
       (let [file-id (/ move-idx 2)
             space-size (nth memory idx)
             move-size (nth memory move-idx)
-            movable-size (min space-size move-size)
-            expanded (repeat movable-size file-id)
+            logical-idx* (min (+ logical-idx space-size) (+ logical-idx move-size))
+            checksum (reduce + (map #(* file-id %1) (range logical-idx logical-idx*)))
             move-idx* (if (>= space-size move-size) (- move-idx 2) move-idx)
             remaining* (if (<= space-size move-size) (- move-size space-size) 0)
             idx* (if (> space-size move-size) idx (inc idx))
             space-size-remaining (if (> space-size move-size) (- space-size move-size) 0)
             memory* (assoc memory move-idx remaining* idx space-size-remaining)]
-        (recur memory* idx* move-idx* (concat expanded-memory expanded))))))
+        (recur memory* logical-idx* idx* move-idx* (+ total checksum))))))
 
 (defn calculate-checksum-for-memory
   "should find solution"
   [data]
-  (println "data=" data)
   (let [memory (into [] (map #(- (int %1) (int \0))) data)]
-    (->>
-     (expand-memory memory)
-     (map-indexed vector)
-     (map (fn [[idx id]] (* idx id)))
-     (reduce +))))
+    (checksum-for-memory memory)))
+
+; TASK 2
+(defn insert-at-index [v index elem]
+  (vec (concat (subvec v 0 index) [elem] (subvec v index))))
+
+(defn calculate-checksum-for-memory-move-blocks
+  "should find solution"
+  [data]
+  (let [memory (into [] (map #(- (int %1) (int \0))) data)
+        memory-mapped (->> memory
+                           (map-indexed vector)
+                           (map (fn [[id size]] (if (even? id) [(/ id 2) size] [-1 size])))
+                           (into []))]
+
+    (def writeable-memory (atom memory-mapped))
+
+    (reduce (fn [_ [file-id file-size]]
+              (let
+               [next-free-space (first (filter (fn [[space-id space-size]] (and (= space-id -1) (>= space-size file-size))) @writeable-memory))
+                space-position (.indexOf @writeable-memory next-free-space)
+                file-position (.lastIndexOf @writeable-memory [file-id file-size])]
+                (when (> file-position space-position -1)
+                  (swap! writeable-memory assoc space-position [file-id file-size])
+                  (swap! writeable-memory assoc file-position [-1 file-size])
+                  (swap! writeable-memory
+                         #(insert-at-index % (inc space-position) [-1 (- (second next-free-space) file-size)])))))
+            [] (take-nth 2 (reverse memory-mapped))))
+  (->>
+   @writeable-memory
+   (map (fn [[id size]] (if (> id -1) (repeat size id) (repeat size 0))))
+   (flatten)
+   (map-indexed (fn [index id] (* index id)))
+   (reduce +)))
+
+
+
--- test/com/day9/Task_test.clj
+++ test/com/day9/Task_test.clj
@@ -14,3 +14,11 @@
 (deftest should-calculate-checksum-for-memory-in-input-file
   (testing "Should calculate checksum for memory in input file"
     (is (= 6461289671426 (calculate-checksum-for-memory (slurp data-file))))))
+
+(deftest should-calculate-checksum-when-moving-block-memory-in-example
+  (testing "Should calculate checksum for memory in example"
+    (is (= 2858 (calculate-checksum-for-memory-move-blocks "2333133121414131402")))))
+
+(deftest should-calculate-checksum-when-moving-block-memory-in-file
+  (testing "Should calculate checksum for memory in example"
+    (is (= 6488291456470 (calculate-checksum-for-memory-move-blocks (slurp data-file))))))
