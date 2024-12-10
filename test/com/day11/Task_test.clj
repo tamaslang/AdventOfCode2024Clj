8fb02409134f59e9a78f467f86af7675bed6a50f
Tamas <tamas@talangsoft.org>
2024-12-10 17:49:46 +0100

    Day 11 skeleton

---
+++ test/com/day11/Task_test.clj
@@ -0,0 +1,16 @@
+(ns com.day11.Task-test
+  (:require [clojure.test :refer :all]
+            [com.day11.Task :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def data-file (io/resource
+                "resources/day11/input.txt"))
+
+(deftest should-solve-in-example
+  (testing "Should solve"
+    (is (= 0 (solve ["3   4"])))))
+
+(deftest should-solve-for-input-file
+  (testing "Should solve for input file"
+    (is (= 0 (solve (str/split-lines (slurp data-file)))))))
