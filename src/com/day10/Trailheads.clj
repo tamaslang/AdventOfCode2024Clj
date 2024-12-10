5d4dd9463485a6c7a004400cefb3a07841fdebc2
Tamas <tamas@talangsoft.org>
2024-12-10 08:38:51 +0100

    Day 10 - task2 solved

--- src/com/day10/Trailheads.clj
+++ src/com/day10/Trailheads.clj
@@ -10,10 +10,8 @@
                   ; row below
                   (matrix->get-char-at-xy matrix [x (inc y)])]))
 
-(defn find-end-from [matrix starting-position]
+(defn find-unique-end-from [matrix starting-position]
   (set (reduce (fn [next-positions next-slope]
-                 (println "next slope " next-slope)
-                 (println "next-positions " next-positions)
                  (let
                   [adjacents (mapcat #(matrix->adjacents matrix %) next-positions)
                    next-positions (filter (fn [[height _]] (= height next-slope)) adjacents)]
@@ -28,6 +26,30 @@
     starting-points (matrix->find-all-nr-with-fmatching trailmap #(= % \0))]
     (->>
      starting-points
-     (map #(find-end-from trailmap %1))
+     (map #(find-unique-end-from trailmap %1))
+     (map count)
+     (reduce +))))
+
+(defn map-route-to-next-slope [matrix route next-slope]
+  (let [adjacents (matrix->adjacents matrix (last route))
+        valid-adjacents (filter (fn [[height _]] (= height next-slope)) adjacents)
+        branched-routes (map #(conj route %) valid-adjacents)]
+    branched-routes))
+
+(defn find-distinct-routes-from [matrix starting-position]
+  (set (reduce
+        (fn [routes next-slope] (mapcat (fn [route] (map-route-to-next-slope matrix route next-slope)) routes))
+        [[starting-position]]
+        "123456789")))
+
+(defn find-trailheads-scores
+  "should find trailheads"
+  [data]
+  (let
+   [trailmap (create-matrix data)
+    starting-points (matrix->find-all-nr-with-fmatching trailmap #(= % \0))]
+    (->>
+     starting-points
+     (map #(find-distinct-routes-from trailmap %1))
      (map count)
      (reduce +))))
--- test/com/day10/Trailheads_test.clj
+++ test/com/day10/Trailheads_test.clj
@@ -7,8 +7,8 @@
 (def data-file (io/resource
                 "resources/day10/input.txt"))
 
-(deftest should-solve-in-example
-  (testing "Should solve"
+(deftest should-find-trailheads-in-example
+  (testing "Should find trailheads in example"
     (is (= 36 (find-trailheads ["89010123"
                                 "78121874"
                                 "87430965"
@@ -18,6 +18,54 @@
                                 "01329801"
                                 "10456732"])))))
 
-(deftest should-solve-for-input-file
-  (testing "Should solve for input file"
+(deftest should-find-trailheads-in-input-file
+  (testing "Should find trailheads in input file"
     (is (= 816 (find-trailheads (str/split-lines (slurp data-file)))))))
+
+(deftest should-find-trailheads-with-distinct-route-in-example-1
+  (testing "Should find trailheads with distinct route in example 1"
+    (is (= 3 (find-trailheads-scores
+              [".....0."
+               "..4321."
+               "..5..2."
+               "..6543."
+               "..7..4."
+               "..8765."
+               "..9...."])))))
+
+(deftest should-find-trailheads-with-distinct-route-in-example-2
+  (testing "Should find trailheads with distinct route in example 2"
+    (is (= 13 (find-trailheads-scores
+               ["..90..9"
+                "...1.98"
+                "...2..7"
+                "6543456"
+                "765.987"
+                "876...."
+                "987...."])))))
+
+(deftest should-find-trailheads-with-distinct-route-in-example-3
+  (testing "Should find trailheads with distinct route in example 3"
+    (is (= 227 (find-trailheads-scores
+                ["012345"
+                 "123456"
+                 "234567"
+                 "345678"
+                 "4.6789"
+                 "56789."])))))
+
+(deftest should-find-trailheads-with-distinct-route-in-example-4
+  (testing "Should find trailheads with distinct route in example 4"
+    (is (= 81 (find-trailheads-scores
+               ["89010123"
+                "78121874"
+                "87430965"
+                "96549874"
+                "45678903"
+                "32019012"
+                "01329801"
+                "10456732"])))))
+
+(deftest should-find-trailheads-in-input-file
+  (testing "Should find trailheads in input file"
+    (is (= 1960 (find-trailheads-scores (str/split-lines (slurp data-file)))))))
