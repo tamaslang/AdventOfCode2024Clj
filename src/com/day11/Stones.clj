2fc5db3bdb785062bf0aff8b158ee2b09408030b
Tamas <tamas@talangsoft.org>
2024-12-11 08:55:03 +0100

    day 11 solve for task 1 - refactor

--- src/com/day11/Stones.clj
+++ src/com/day11/Stones.clj
@@ -1,10 +1,24 @@
 (ns com.day11.Stones
   (:require [com.utils.InputParsing :refer :all]))
 
+
+(defn count-digits [number]
+  (loop
+    [remaining number
+     digit-count 1]
+    (if (= (long (/ remaining 10)) 0) digit-count (recur (long (/ remaining 10)) (inc digit-count)))
+    )
+  )
+
+(defn split-nr-at-digit [number digit]
+  [(long (/ number (Math/pow 10 digit))) (long (mod number (Math/pow 10 digit)))]
+  )
+
+
 (defn apply-rule [stone]
   (cond
     (= stone 0) [1]
-    (even? (count (str stone))) (map Integer/parseInt (map (partial apply str) (partition-all (/ (count (str stone)) 2) (str stone))))
+    (even? (count-digits stone))  (split-nr-at-digit stone (/ (count-digits stone) 2))
     :else [(* 2024 stone)]
     )
   )
@@ -13,8 +27,9 @@
   "should find solution"
   [blink stones]
   (let [stones (parse-numbers-in-line stones)]
-    (count (reduce (fn [all-stones _]
-              (flatten (map apply-rule all-stones)))
-            stones (range 0 blink) ))
-  )
+    (count (reduce (fn [all-stones blink]
+              (println "blink=" blink " hell? " (count all-stones))
+              (flatten (map apply-rule all-stones))
+              )
+            (into [] stones) (range 0 blink))))
   )
--- test/com/day11/Stones_test.clj
+++ test/com/day11/Stones_test.clj
@@ -11,10 +11,18 @@
   (testing "Should solve"
     (is (= 22 (blink 6 "125 17")))))
 
-(deftest should-solve-in-example
+(deftest should-solve-in-example-2
   (testing "Should solve"
     (is (= 55312 (blink 25 "125 17")))))
 
 (deftest should-solve-for-input-file
   (testing "Should solve for input file"
     (is (= 199946 (blink 25 (slurp data-file))))))
+
+;(deftest should-solve-for-input-file-when-blink-75-times
+;  (testing "Should solve for input file when blink 72 times"
+;    (is (= 199946 (blink 75 (slurp data-file))))))
+;
+;(deftest should-solve-in-example-0
+;  (testing "Should solve"
+;    (is (= 11 (blink 10 "0")))))
