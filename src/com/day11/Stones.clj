9e7e2a1590a6de55452e301fa299bbbf2021fa36
Tamas <tamas@talangsoft.org>
2024-12-11 09:20:45 +0100

    day 11 solve for task 2

--- src/com/day11/Stones.clj
+++ src/com/day11/Stones.clj
@@ -1,35 +1,35 @@
 (ns com.day11.Stones
   (:require [com.utils.InputParsing :refer :all]))
 
-
 (defn count-digits [number]
   (loop
-    [remaining number
-     digit-count 1]
-    (if (= (long (/ remaining 10)) 0) digit-count (recur (long (/ remaining 10)) (inc digit-count)))
-    )
-  )
+   [remaining number
+    digit-count 1]
+    (if (= (long (/ remaining 10)) 0) digit-count (recur (long (/ remaining 10)) (inc digit-count)))))
 
 (defn split-nr-at-digit [number digit]
-  [(long (/ number (Math/pow 10 digit))) (long (mod number (Math/pow 10 digit)))]
-  )
-
+  [(long (/ number (Math/pow 10 digit))) (long (mod number (Math/pow 10 digit)))])
 
 (defn apply-rule [stone]
   (cond
     (= stone 0) [1]
     (even? (count-digits stone))  (split-nr-at-digit stone (/ (count-digits stone) 2))
-    :else [(* 2024 stone)]
-    )
-  )
+    :else [(* 2024 stone)]))
+
+(def count-mapped-stone-memo
+  (memoize
+   (fn [stone blinks]
+     (loop
+      [stones [stone]
+       remaining-blinks blinks]
+       (if (zero? remaining-blinks)
+         (count stones)
+         (reduce + (map #(count-mapped-stone-memo % (dec remaining-blinks)) (mapcat apply-rule stones))))))))
+
+(defn recur-blink [stones blink]
+  (reduce + (map #(count-mapped-stone-memo % blink) stones)))
 
 (defn blink
   "should find solution"
   [blink stones]
-  (let [stones (parse-numbers-in-line stones)]
-    (count (reduce (fn [all-stones blink]
-              (println "blink=" blink " hell? " (count all-stones))
-              (flatten (map apply-rule all-stones))
-              )
-            (into [] stones) (range 0 blink))))
-  )
+  (recur-blink (parse-numbers-in-line stones) blink))
--- test/com/day11/Stones_test.clj
+++ test/com/day11/Stones_test.clj
@@ -1,7 +1,6 @@
 (ns com.day11.Stones-test
   (:require [clojure.test :refer :all]
             [com.day11.Stones :refer :all]
-            [clojure.string :as str]
             [clojure.java.io :as io]))
 
 (def data-file (io/resource
@@ -19,10 +18,6 @@
   (testing "Should solve for input file"
     (is (= 199946 (blink 25 (slurp data-file))))))
 
-;(deftest should-solve-for-input-file-when-blink-75-times
-;  (testing "Should solve for input file when blink 72 times"
-;    (is (= 199946 (blink 75 (slurp data-file))))))
-;
-;(deftest should-solve-in-example-0
-;  (testing "Should solve"
-;    (is (= 11 (blink 10 "0")))))
+(deftest should-solve-for-input-file-when-blink-75-times
+  (testing "Should solve for input file when blink 72 times"
+    (is (= 237994815702032 (blink 75 (slurp data-file))))))
