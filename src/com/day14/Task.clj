f4f635a0f24aed9d80a1212726b6500cdfdbe8a3
Tamas <tamas@talangsoft.org>
2024-12-14 07:06:48 +0100

    Day14 solve for part 1 / simplify

--- src/com/day14/Task.clj
+++ src/com/day14/Task.clj
@@ -2,33 +2,19 @@
   (:require [com.utils.InputParsing :refer :all]))
 
 (defn parse-robot-state [robot-state]
-  (map Integer/parseInt (rest (re-find #"p=(-?\d+),(-?\d+)\sv=(-?\d+),(-?\d+)" robot-state)))
-)
+  (map Integer/parseInt (rest (re-find #"p=(-?\d+),(-?\d+)\sv=(-?\d+),(-?\d+)" robot-state))))
 
 (defn move-robot-for-seconds [seconds [px py vx vy] [X Y]]
-  [
-   (mod (+ px (* seconds vx)) X)
-   (mod (+ py (* seconds vy)) Y)
-  ]
-)
+  [(mod (+ px (* seconds vx)) X)
+   (mod (+ py (* seconds vy)) Y)])
 
 (defn solve [seconds [X Y] data]
-  (println "data" data)
-  (let [
-        robots (map parse-robot-state data)
+  (let [robots (map parse-robot-state data)
         robot-position-after-seconds (map #(move-robot-for-seconds seconds % [X Y]) robots)
         quadrant-size-x (int (/ X 2)) ; 5
         quadrant-size-y (int (/ Y 2)) ; 3
-        quadrant-1-bots (count (filter (fn[[px py]] (and (< px quadrant-size-x) (< py quadrant-size-y))) robot-position-after-seconds))
-        quadrant-2-bots (count (filter (fn[[px py]] (and (< quadrant-size-x px ) (< py quadrant-size-y))) robot-position-after-seconds))
-        quadrant-3-bots (count (filter (fn[[px py]] (and  (<  quadrant-size-x px) (< quadrant-size-y py))) robot-position-after-seconds))
-        quadrant-4-bots (count (filter (fn[[px py]] (and (<  px quadrant-size-x ) (<  quadrant-size-y py))) robot-position-after-seconds))
-        ]
-    (* quadrant-1-bots quadrant-2-bots quadrant-3-bots quadrant-4-bots)
-  )
-  ;(->>
-  ;  data
-  ;  parse-robot-state
-  ;  (map #(move-robot-for-seconds seconds % [X Y]))
-  ;  )
-)
+        quadrant-1-bots (filter (fn [[px py]] (and (< px quadrant-size-x) (< py quadrant-size-y))) robot-position-after-seconds)
+        quadrant-2-bots (filter (fn [[px py]] (and (< quadrant-size-x px) (< py quadrant-size-y))) robot-position-after-seconds)
+        quadrant-3-bots (filter (fn [[px py]] (and  (<  quadrant-size-x px) (< quadrant-size-y py))) robot-position-after-seconds)
+        quadrant-4-bots (filter (fn [[px py]] (and (<  px quadrant-size-x) (<  quadrant-size-y py))) robot-position-after-seconds)]
+    (reduce * (map count [quadrant-1-bots quadrant-2-bots quadrant-3-bots quadrant-4-bots]))))
--- test/com/day14/Task_test.clj
+++ test/com/day14/Task_test.clj
@@ -9,28 +9,23 @@
 
 (deftest should-solve-in-example-0
   (testing "Should solve"
-    (is (= 0 (solve 100 [11 7] [
-                                "p=2,4 v=2,-3"
-                                ])))))
-
+    (is (= 0 (solve 100 [11 7] ["p=2,4 v=2,-3"])))))
 
 (deftest should-solve-in-example-1
   (testing "Should solve"
-    (is (= 0 (solve 100 [11 7] [
-                     "p=0,4 v=3,-3"
-                     "p=6,3 v=-1,-3"
-                     "p=10,3 v=-1,2"
-                     "p=2,0 v=2,-1"
-                     "p=0,0 v=1,3"
-                     "p=3,0 v=-2,-2"
-                     "p=7,6 v=-1,-3"
-                     "p=3,0 v=-1,-2"
-                     "p=9,3 v=2,3"
-                     "p=7,3 v=-1,2"
-                     "p=2,4 v=2,-3"
-                     "p=9,5 v=-3,-3"
-                     ])))))
+    (is (= 0 (solve 100 [11 7] ["p=0,4 v=3,-3"
+                                "p=6,3 v=-1,-3"
+                                "p=10,3 v=-1,2"
+                                "p=2,0 v=2,-1"
+                                "p=0,0 v=1,3"
+                                "p=3,0 v=-2,-2"
+                                "p=7,6 v=-1,-3"
+                                "p=3,0 v=-1,-2"
+                                "p=9,3 v=2,3"
+                                "p=7,3 v=-1,2"
+                                "p=2,4 v=2,-3"
+                                "p=9,5 v=-3,-3"])))))
 
 (deftest should-solve-for-input-file
   (testing "Should solve for input file"
-    (is (= 0 (solve 100 [101 103] (str/split-lines (slurp data-file)))))))
+    (is (= 210587128 (solve 100 [101 103] (str/split-lines (slurp data-file)))))))
