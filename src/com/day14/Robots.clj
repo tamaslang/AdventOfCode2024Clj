67edcdc0a7f032eccfef2127d58c49c5a0ceca1d
Tamas <tamas@talangsoft.org>
2024-12-14 13:42:57 +0100

    Day14 attempting to solve for part 2 WIP / still running for too long

--- src/com/day14/Robots.clj
+++ src/com/day14/Robots.clj
@@ -1,4 +1,4 @@
-(ns com.day14.Task
+(ns com.day14.Robots
   (:require [clojure.set :as set]
             [com.utils.InputParsing :refer :all]))
 
@@ -9,7 +9,7 @@
   [(mod (+ px (* seconds vx)) X)
    (mod (+ py (* seconds vy)) Y)])
 
-(defn solve [seconds [X Y] data]
+(defn count-robots-in-quadrants-after [seconds [X Y] data]
   (let [robots (map parse-robot-state data)
         robot-position-after-seconds (map #(move-robot-for-seconds seconds % [X Y]) robots)
         quadrant-size-x (int (/ X 2)) ; 5
@@ -21,24 +21,20 @@
     (reduce * (map count [quadrant-1-bots quadrant-2-bots quadrant-3-bots quadrant-4-bots]))))
 
 (defn is-symmetric? [quadrant-a quadrant-b [X Y]]
-  (every? (fn[[x y]] (not-empty (set/intersection #{[(- X 1 x) y]} quadrant-b))) quadrant-a)
-)
+  (every? (fn [[x y]] (not-empty (set/intersection #{[(- X 1 x) y]} quadrant-b))) quadrant-a))
 
 (defn is-symmetric-horizontally? [robot-positions X]
-  (every? (fn[[x y]] (not-empty (set/intersection #{[(- X 1 x) y]} robot-positions))) robot-positions)
-)
+  (every? (fn [[x y]] (not-empty (set/intersection #{[(- X 1 x) y]} robot-positions))) robot-positions))
 ;(let [
 ;      threshold 10]
 ;  (every? (fn [[x y]] (contains? robot-positions [(- X 1 x) y])) (take threshold robot-positions))
 ;  )
 
 (defn find-symmetric [quadrant-a quadrant-b [X Y]]
-  (filter (fn[[x y]] (not-empty (set/intersection #{[(- X 1 x) y]} quadrant-b))) quadrant-a)
-)
+  (filter (fn [[x y]] (not-empty (set/intersection #{[(- X 1 x) y]} quadrant-b))) quadrant-a))
 
 (defn is-symmetric-v2? [quadrant-a quadrant-b [X Y]]
-  (> (count (find-symmetric quadrant-a quadrant-b [X Y])) (/ (count quadrant-a) 2))
-)
+  (> (count (find-symmetric quadrant-a quadrant-b [X Y])) (/ (count quadrant-a) 2)))
 
 (defn is-christmas-tree? [robot-positions [X Y]]
   (let [quadrant-size-x (int (/ X 2))
@@ -58,27 +54,24 @@
 (defn print-tree [coordinates [X Y]]
   (reduce (fn [_, count]
             (let
-              [[x y] [(mod count X) (int (Math/floor (/ count X)))]]
+             [[x y] [(mod count X) (int (Math/floor (/ count X)))]]
               ;(println [x y])
               (when (zero? x) (println ""))
-              (if (empty? (set/intersection coordinates #{[x y]})) (print \.) (print \0))
-              )
-            )
-          []
-          (range 0  (* X Y)))
-  )
+              (if (empty? (set/intersection coordinates #{[x y]})) (print \.) (print \0))))
 
-(defn solve-for-christmas-tree [from-seconds max-seconds [X Y] data]
+          []
+          (range 0  (* X Y))))
+
+(defn find-christmas-tree [from-seconds max-seconds [X Y] data]
   (let [robots (map parse-robot-state data)]
     (reduce (fn [_, second]
               (let
                [robots-moved (map #(move-robot-for-seconds second % [X Y]) robots)
                 christmas-tree? (is-christmas-tree? robots-moved [X Y])
                 quadrant-size-x (int (/ X 2))
-                quadrant-size-y (int (/ Y 2))
-                ]
+                quadrant-size-y (int (/ Y 2))]
                 (when (zero? (mod second 10000)) (println "not found at =" second))
-                (when christmas-tree? (println "FOUND AT SECONDS=" second) )
+                (when christmas-tree? (println "FOUND AT SECONDS=" second))
                 (when christmas-tree? (print-tree (set robots-moved) [quadrant-size-x quadrant-size-y]))
                 (if christmas-tree? (reduced second) 0)))
             0
---
+++ test/com/day14/Robots_test.clj
@@ -0,0 +1,50 @@
+(ns com.day14.Robots-test
+  (:require [clojure.test :refer :all]
+            [com.day14.Robots :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def data-file (io/resource
+                "resources/day14/input.txt"))
+
+(deftest should-find-quadrant-sizes-in-example-1
+  (testing "Should find quadrant sizes in example 1"
+    (is (= 12 (count-robots-in-quadrants-after 100 [11 7] ["p=0,4 v=3,-3"
+                                               "p=6,3 v=-1,-3"
+                                               "p=10,3 v=-1,2"
+                                               "p=2,0 v=2,-1"
+                                               "p=0,0 v=1,3"
+                                               "p=3,0 v=-2,-2"
+                                               "p=7,6 v=-1,-3"
+                                               "p=3,0 v=-1,-2"
+                                               "p=9,3 v=2,3"
+                                               "p=7,3 v=-1,2"
+                                               "p=2,4 v=2,-3"
+                                               "p=9,5 v=-3,-3"])))))
+
+(deftest should-find-quadrant-sizes-for-input-file
+  (testing "Should find quadrant sizes in input file"
+    (is (= 210587128 (count-robots-in-quadrants-after 100 [101 103] (str/split-lines (slurp data-file)))))))
+
+(deftest should-find-christmas-tree-for-input-file
+  (testing "Should find christmas tree in input file"
+    (is (= 0 (find-christmas-tree 280000 40000000 [101 103] (str/split-lines (slurp data-file)))))))
+
+(deftest should-find-christmas-tree-in-example
+  (testing "Should find christmas tree in example"
+    (is (= 0 (find-christmas-tree 0 100 [11 7] ["p=0,4 v=3,-3"
+                                                "p=6,3 v=-1,-3"
+                                                "p=10,3 v=-1,2"
+                                                "p=2,0 v=2,-1"
+                                                "p=0,0 v=1,3"
+                                                "p=3,0 v=-2,-2"
+                                                "p=7,6 v=-1,-3"
+                                                "p=3,0 v=-1,-2"
+                                                "p=9,3 v=2,3"
+                                                "p=7,3 v=-1,2"
+                                                "p=2,4 v=2,-3"
+                                                "p=9,5 v=-3,-3"])))))
+; not found at = 6000000
+; not found at = 6670000
+;not found at = 7000000
+; less than 10000000 ?
