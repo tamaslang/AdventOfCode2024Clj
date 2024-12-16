3077e3b9efbe7c80ee64f10411cb4b3d413efb1b
Tamas <tamas@talangsoft.org>
2024-12-16 14:32:50 +0100

    Day 16 solve for part 2

--- src/com/day16/ReindeerMaze.clj
+++ src/com/day16/ReindeerMaze.clj
@@ -1,5 +1,6 @@
 (ns com.day16.ReindeerMaze
-  (:require [com.utils.InputParsing :refer :all]))
+  (:require [clojure.set :as set]
+            [com.utils.InputParsing :refer :all]))
 
 (defn adjacents [matrix [x y]]
   (keep identity [;above
@@ -53,27 +54,28 @@
 
 (defn find-reindeer-path [upper-limit matrix start-pos end-pos]
   (def visited-pos (atom {}))
-  (reduce (fn [reindeers _]
-            (if (empty? reindeers) (reduced @visited-pos)
-                (mapcat (fn [{:keys [pos direction score]}]
-                          (let
-                           [reached-end? (end? (matrix->get-xy matrix pos))]
-                                               ; UPDATE VISITED
-                            (when (not (was-visited-cheaper @visited-pos pos direction score)) (swap! visited-pos assoc [pos direction] score))
-                            (cond
-                              reached-end? []
-                              (was-visited-cheaper @visited-pos pos direction score) []
-                              :else
-                              (apply-upper-score-limit upper-limit
-                                                       (all-positions-to-move-from-here matrix pos (go-forward direction) score)))))
+  (let [[reindeers steps] (reduce (fn [reindeers step-count]
+                                    (if (empty? reindeers) (reduced [reindeers step-count])
+                                        (mapcat (fn [{:keys [pos direction score]}]
+                                                  (let
+                                                   [reached-end? (end? (matrix->get-xy matrix pos))]
+                                                                       ; UPDATE VISITED
+                                                    (when (not (was-visited-cheaper @visited-pos pos direction score)) (swap! visited-pos assoc [pos direction] score))
+                                                    (cond
+                                                      reached-end? []
+                                                      (was-visited-cheaper @visited-pos pos direction score) []
+                                                      :else
+                                                      (apply-upper-score-limit upper-limit
+                                                                               (all-positions-to-move-from-here matrix pos (go-forward direction) score)))))
 
-                        reindeers)))
-          [{:pos start-pos :direction [1 0] :score 0}] (range))
-  (remove nil?
-          [(@visited-pos [end-pos [0, -1]])
-           (@visited-pos [end-pos [1 0]])
-           (@visited-pos [end-pos [0 1]])
-           (@visited-pos [end-pos [-1 0]])]))
+                                                reindeers)))
+                                  [{:pos start-pos :direction [1 0] :score 0}] (range))]
+    (println "finished in steps=" steps)
+    (remove nil?
+            [(@visited-pos [end-pos [0, -1]])
+             (@visited-pos [end-pos [1 0]])
+             (@visited-pos [end-pos [0 1]])
+             (@visited-pos [end-pos [-1 0]])])))
 
 (defn find-path-with-lowest-score
   "should find solution"
@@ -82,3 +84,58 @@
         start-pos (matrix->find-first matrix \S)
         end-pos (matrix->find-first matrix \E)]
     (apply min (find-reindeer-path upper-limit matrix start-pos end-pos))))
+
+; TASK 2
+;
+;(defn traverse-from-position [upper-limit matrix visited-pos pos direction score visited]
+;  (cond
+;    (end? (matrix->get-xy matrix pos)) [score visited]
+;    (was-visited-cheaper @visited-pos pos direction score) nil
+;    :else
+;      (let
+;        [
+;         visited* (conj visited pos)
+;         all-pos-to-move (apply-upper-score-limit upper-limit (all-positions-to-move-from-here matrix pos (go-forward direction) score))]
+;        (swap! visited-pos assoc [pos direction] score)
+;        (remove nil? (mapcat (fn [{:keys [pos direction score]}] (traverse-from-position upper-limit matrix visited-pos pos direction score visited*)) all-pos-to-move))
+;        )
+;    )
+;  )
+;
+;(defn count-fields-best-path [upper-limit matrix start-pos end-pos]
+;  (def visited-pos (atom {}))
+;  (println "ALL VISITED=" (traverse-from-position upper-limit matrix visited-pos start-pos [1 0] 0 #{}))
+;  ;(println "ALL VISITED=" (filter (fn[[score positions]]  (nil? score)) (traverse-from-position upper-limit matrix visited-pos start-pos [1 0] 0 #{})))
+;  (println "FINISHED")
+;  )
+
+(defn all-reindeers-reached-end? [matrix reindeers]
+  (every? (fn [{:keys [pos]}] (end? (matrix->get-xy matrix pos))) reindeers))
+
+(defn count-fields-best-path [upper-limit matrix start-pos end-pos]
+  (def visited-pos (atom {}))
+  (let [[reindeers steps] (reduce (fn [reindeers step-count]
+                                    (if (all-reindeers-reached-end? matrix reindeers) (reduced [reindeers step-count])
+                                        (mapcat (fn [{:keys [pos direction score visited]}]
+                                                                       ; UPDATE VISITED
+                                                  (when (not (was-visited-cheaper @visited-pos pos direction score)) (swap! visited-pos assoc [pos direction] score))
+                                                  (cond
+                                                    (was-visited-cheaper @visited-pos pos direction score) []
+                                                    :else
+                                                    (->>
+                                                     (all-positions-to-move-from-here matrix pos (go-forward direction) score)
+                                                     (filter (fn [{:keys [pos direction score]}] (<= score upper-limit)))
+                                                     (map #(assoc % :visited (conj visited pos (:pos %)))))))
+
+                                                reindeers)))
+                                  [{:pos start-pos :direction [1 0] :score 0 :visited #{}}] (range))]
+    (count (apply set/union (map (fn [{:keys [pos direction score visited]}] visited) reindeers)))))
+
+(defn find-tiles-for-seats
+  "should find solution"
+  [upper-limit data]
+  (let [matrix (create-matrix data)
+        start-pos (matrix->find-first matrix \S)
+        end-pos (matrix->find-first matrix \E)]
+    (count-fields-best-path upper-limit matrix start-pos end-pos)))
+
--- test/com/day16/ReindeerMaze_test.clj
+++ test/com/day16/ReindeerMaze_test.clj
@@ -9,7 +9,7 @@
 
 (deftest should-find=path-with-lowest-score-in-small-example
   (testing "Should find path with lowest score"
-    (is (= 1004 (find-path-with-lowest-score 7036 ["#####"
+    (is (= 1004 (find-path-with-lowest-score 1005 ["#####"
                                                    "#..E#"
                                                    "#.#.#"
                                                    "#S..#"
@@ -57,3 +57,52 @@
   (testing "Should find path with lowest score in input file"
     (is (= 85396 (find-path-with-lowest-score 85396 (str/split-lines (slurp data-file)))))))
 
+(deftest should-find-seats-in-small-example
+  (testing "Should find seats in small example"
+    (is (= 8 (find-tiles-for-seats Integer/MAX_VALUE ["#####"
+                                                      "#..E#"
+                                                      "#.#.#"
+                                                      "#S..#"
+                                                      "#####"])))))
+
+(deftest should-find-saets-in-example-1
+  (testing "Should find path with lowest score"
+    (is (= 45 (find-tiles-for-seats 7036 ["###############"
+                                          "#.......#....E#"
+                                          "#.#.###.#.###.#"
+                                          "#.....#.#...#.#"
+                                          "#.###.#####.#.#"
+                                          "#.#.#.......#.#"
+                                          "#.#.#####.###.#"
+                                          "#...........#.#"
+                                          "###.#.#####.#.#"
+                                          "#...#.....#.#.#"
+                                          "#.#.#.###.#.#.#"
+                                          "#.....#...#.#.#"
+                                          "#.###.#.#.#.#.#"
+                                          "#S..#.....#...#"
+                                          "###############"])))))
+
+(deftest should-find-saets-in-example-2
+  (testing "Should find path with lowest score"
+    (is (= 64 (find-tiles-for-seats 12000 ["#################"
+                                           "#...#...#...#..E#"
+                                           "#.#.#.#.#.#.#.#.#"
+                                           "#.#.#.#...#...#.#"
+                                           "#.#.#.#.###.#.#.#"
+                                           "#...#.#.#.....#.#"
+                                           "#.#.#.#.#.#####.#"
+                                           "#.#...#.#.#.....#"
+                                           "#.#.#####.#.###.#"
+                                           "#.#.#.......#...#"
+                                           "#.#.###.#####.###"
+                                           "#.#.#...#.....#.#"
+                                           "#.#.#.#####.###.#"
+                                           "#.#.#.........#.#"
+                                           "#.#.#.#########.#"
+                                           "#S#.............#"
+                                           "#################"])))))
+
+(deftest should-find-seats-in-input-file
+  (testing "Should find seats in input file"
+    (is (= 428 (find-tiles-for-seats 85396 (str/split-lines (slurp data-file)))))))
