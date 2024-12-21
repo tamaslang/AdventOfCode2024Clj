63a16064834922ae965b4cbf52ab6ee243520622
Tamas <tamas@talangsoft.org>
2024-12-21 23:33:31 +0100

    Day 21 - part1 (optimised)

--- src/com/day21/Numpad.clj
+++ src/com/day21/Numpad.clj
@@ -1,13 +1,11 @@
 (ns com.day21.Numpad
   (:require [com.utils.InputParsing :refer :all]))
 
-
 (defn distance [from to]
   (mapv - to from))
 
 (def numpad
-  {
-   \7 [0 0]
+  {\7 [0 0]
    \8 [1 0]
    \9 [2 0]
    \4 [0 1]
@@ -18,55 +16,63 @@
    \3 [2 2]
    \X [0 3]
    \0 [1 3]
-   \A [2 3]
-   }
-  )
+   \A [2 3]})
 
 (def dirpad
+  {\X [0 0]
+   \^ [1 0]
+   \A [2 0]
+   \< [0 1]
+   \v [1 1]
+   \> [2 1]})
+
+
+
+;Least turns (this becomes important when escaping the missing cell in both numeric and directional pads).
+;
+;moving < over ^ over v over >.
+(def move-order
   {
-    \X [0 0]
-    \^ [1 0]
-    \A [2 0]
-    \< [0 1]
-    \v [1 1]
-    \> [2 1]
+   \< 0
+   \^ 1
+   \v 2
+   \> 3
    }
-)
+  )
+(defn instructions-from-to[keypad-mapped from to]
+  (let [[from to] [(keypad-mapped from) (keypad-mapped to)]
+        void (keypad-mapped \X)
+        [dx dy] (distance from to)
+        x-instructions (if (< dx 0) (repeat (abs dx) \<) (repeat dx \>))
+        y-instructions (if (< dy 0) (repeat (abs dy) \^) (repeat dy \v))
+        paths    (->> [(when (not= void [(+ (first from) dx) (second from)]) ; can go x first
+                (str (apply str x-instructions) (apply str y-instructions) "A"))
+              (when (not= void [(first from) (+ dy (second from))]) ; can go y first
+                (str (apply str y-instructions) (apply str x-instructions) "A"))]
+             (keep identity))
+        ]
+    (first (sort-by (fn[path] (move-order (first path))) paths))
+    ))
 
 (defn robot-sequence [keypad-mapped sequence]
   (reduce (fn [instructions [from to]]
-            (let [[dx dy] (distance (keypad-mapped from) (keypad-mapped to))]
-               (str
-                         instructions
-                         (apply str (if (< dy 0) (repeat (abs dy) \^) (repeat dy \v)))
-                         (apply str (if (< dx 0) (repeat (abs dx) \<) (repeat dx \>)))
-                         \A
-                         )
-              )
-            )
+            (str instructions (instructions-from-to keypad-mapped from to)))
           ""
-          (partition 2 1 (str "A" sequence))
-  )
-)
+          (partition 2 1 (str "A" sequence))))
 
 (defn to-instructions [input]
   (->> [input]
        (map #(robot-sequence numpad %))
        (map #(robot-sequence dirpad %))
        (map #(robot-sequence dirpad %))
-       first
-       )
-  )
+       first))
 
 (defn numeric-part [input]
-  (Integer/parseInt (apply str (filter Character/isDigit input)))
-  )
+  (Integer/parseInt (apply str (filter Character/isDigit input))))
 
 (defn calculate-robot-sequences
   "should calculate root sequences"
   [data]
   (->> data
-       (map (fn[input] (* (count (to-instructions input)) (numeric-part input))))
-       (reduce +)
-    )
-  )
+       (map (fn [input] (* (count (to-instructions input)) (numeric-part input))))
+       (reduce +)))
--- test/com/day21/Numpad_test.clj
+++ test/com/day21/Numpad_test.clj
@@ -5,18 +5,16 @@
             [clojure.java.io :as io]))
 
 (def data-file (io/resource
-                 "resources/day21/input.txt"))
+                "resources/day21/input.txt"))
 
 (deftest should-solve-in-example
   (testing "Should solve"
-    (is (= 126384 (calculate-robot-sequences [
-                                         "029A"
-                                         "980A"
-                                         "179A"
-                                         "456A"
-                                         "379A"
-                                         ])))))
+    (is (= 126384 (calculate-robot-sequences ["029A"
+                                              "980A"
+                                              "179A"
+                                              "456A"
+                                              "379A"])))))
 
 (deftest should-solve-for-input-file
   (testing "Should solve for input file"
-    (is (= 0 (calculate-robot-sequences (str/split-lines (slurp data-file)))))))
+    (is (= 184180 (calculate-robot-sequences (str/split-lines (slurp data-file)))))))
